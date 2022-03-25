7957c5c30569045653b81800b3cd64fc5751f28d
cs-iliev <tsvetomir.iliev@singlet.dev>
2022-03-25 14:23:37 +0200

    Add display trailer functionality

--- src/helpers.js
+++ src/helpers.js
@@ -1,11 +1,13 @@
 // Convert time to hours and minutes
-export const calcTime = time => {
+export const calcTime = time =>
+{
   const hours = Math.floor(time / 60);
   const mins = time % 60;
   return `${hours}h ${mins}m`;
 };
 // Convert a number to money formatting
-export const convertMoney = money => {
+export const convertMoney = money =>
+{
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
@@ -14,7 +16,19 @@ export const convertMoney = money => {
   return formatter.format(money);
 };
 
-export const isPersistedState = stateName => {
+export const isPersistedState = stateName =>
+{
   const sessionState = sessionStorage.getItem(stateName);
   return sessionState && JSON.parse(sessionState);
 };
+
+export const filterTrailerURL = (movie) =>
+{
+  const trailer = movie.videos['results'].filter((e) => { return e['name'] == 'Official Trailer' });
+
+  const YOUTUBE_URL = 'https://www.youtube.com/embed/';
+
+  const trailerURL = (trailer.length > 0) ? `${YOUTUBE_URL}${trailer[0]['key']}` : 'No trailer';
+
+  return trailerURL
+}
